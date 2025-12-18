8dc83fba09024d549cf59ad351691c842b013ce2
Md. Kamrul Hasan <mhgolap11@gmail.com>
2025-12-18 14:31:25 +0600

    Release v2.0.1 build 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.mkhglab.agecalculator"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "2.0"
+        versionCode = 5
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
